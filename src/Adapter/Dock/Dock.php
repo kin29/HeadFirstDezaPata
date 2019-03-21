f9db6701c16608a704482d308af591dff50f2c91
kin29 <kin29.com@gmail.com>
2019-03-21 21:02:59 +0900

    Adapterパターンの追加

---
+++ src/Adapter/Dock/Dock.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HFD\Adapter\Dock;
+
+interface Dock
+{
+    public function quack(): void;
+    public function fly(): void;
+}
---
+++ src/Adapter/Dock/MallerDock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HFD\Adapter\Dock;
+
+class MallerDock implements Dock
+{
+	public function quack(): void
+	{
+        echo "ガーガー\n";
+	}
+	
+	public function fly(): void
+	{
+        echo "飛んでいます\n";
+	}
+}
